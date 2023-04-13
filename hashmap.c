567eb02ae8207b38623a503c633e08c62cb4cab8
Replit user <>
2023-04-13 03:42:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -105,23 +105,19 @@ Pair * searchMap(HashMap * map,  char * key) {
 
   if(map->buckets[indice] == NULL){
     return NULL;
-    
-  }else{
-
-    for(long i=indice ; i < map->capacity + indice ; i++){
-
-      long j = i % map->capacity;
-      if(map->buckets[j] == NULL) return NULL;
-
-      if(map->buckets[j]->key == key){
-
-        map->current = indice;
-        return map->buckets[j];
-      }
-    }
-    
   }
 
+  for(long i=indice ; i < map->capacity + indice ; i++){
+    long j = i % map->capacity;
+    
+    if(map->buckets[j]->key == key){
+      map->current = indice;
+      return map->buckets[j];
+    }
+    
+    if(map->buckets[j] == NULL) return NULL;
+  }
+  return NULL;
 }
 
 Pair * firstMap(HashMap * map) {
